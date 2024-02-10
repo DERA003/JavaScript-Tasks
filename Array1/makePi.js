d093076c8cf484a457a9309f5970e772f6bb68f2
Chidera Umejuru <deraumejuru@gmail.com>
2024-02-10 07:58:14 -0800

    new file added

---
+++ Array1/makePi.js
@@ -0,0 +1,11 @@
+// Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+// Examples
+// makePi() → 3,1,4
+
+function makePi() {
+    let arr = [3]
+    return arr
+}
+console.log(makePi);
+
